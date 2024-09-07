d0e61c9e91b03f4da80412d0ca2d13921f489d59
Chinmay Chaudhary <chinmay.chaudhary@sprinklr.com>
2024-09-07 20:27:07 +0530

    chore

--- src/SliderInput.js
+++ src/SliderInput.js
@@ -10,32 +10,51 @@ export const SliderInput = ({ min, max, onSubmit }) => {
   };
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex gap-4">
-        <input
-          role="slider"
-          type="range"
-          value={value}
-          min={min}
-          max={max}
-          onChange={onChange}
-          className=""
-        />
-        <input
-          placeholder="Enter a Number"
-          role="number"
-          type="number"
-          value={value}
-          min={min}
-          max={max}
-          onChange={onChange}
-          className="border rounded border-solid border-gray-600 w-[140px]"
-        />
+    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="sm:col-span-3">
+        <label
+          htmlFor="slider-input"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Slider Input
+        </label>
+        <div className="mt-2">
+          <input
+            id="slider-input"
+            role="slider"
+            type="range"
+            value={value}
+            min={min}
+            max={max}
+            onChange={onChange}
+            className=""
+          />
+        </div>
+      </div>
+      <div className="sm:col-span-3">
+        <label
+          htmlFor="number-input"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Number Input
+        </label>
+        <div className="mt-2">
+          <input
+            id="number-input"
+            role="number"
+            type="number"
+            value={value}
+            min={min}
+            max={max}
+            onChange={onChange}
+            className="block w-[140px] rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
       </div>
       <button
         role="button"
         onClick={onSubmitClick}
-        className="bg-blue-600 rounded p-2 text-white"
+        className="sm:col-span-6 bg-blue-600 rounded p-2 text-white"
       >
         Submit
       </button>
